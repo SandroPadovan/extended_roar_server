d3b834e25d7862465604ec11fb4e062a967de4b8
SandroPadovan <sandropadovan@bluewin.ch>
2023-11-21 23:35:59 +0100

    fix: store resource fingerprint and syscall with same timestamp in filename

--- api/fingerprint.py
+++ api/fingerprint.py
@@ -1,6 +1,7 @@
 import json
 import os.path
 from http import HTTPStatus
+from datetime import datetime
 
 from flask import Blueprint, request
 
@@ -15,9 +16,11 @@ def report_fingerprint(mac):
 
     resource_fp = json.loads(request.form.get('resource_fp'))
 
+    timestamp = datetime.now()
+
     # resource fingerprint
     write_resource_metrics_to_file(str(resource_fp["rate"]), str(resource_fp["fp"]),
-                                   os.path.join(get_storage_path(), 'resource_fp'), is_multi_fp_collection())
+                                   os.path.join(get_storage_path(), 'resource_fp'), is_multi_fp_collection(), timestamp)
 
     # syscall data
     if "syscalls" not in request.files:
@@ -27,6 +30,6 @@ def report_fingerprint(mac):
         return "syscall file was not sent correctly", HTTPStatus.BAD_REQUEST
 
     write_syscall_metrics_to_file(raw_syscall_file, os.path.join(get_storage_path(), 'syscalls'),
-                                  is_multi_fp_collection())
+                                  is_multi_fp_collection(), timestamp)
 
     return "", HTTPStatus.CREATED
