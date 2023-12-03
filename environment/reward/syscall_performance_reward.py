726df0f11a56695f3dfbd7762045cf1bec8b02a6
SandroPadovan <sandropadovan@bluewin.ch>
2023-12-03 21:43:16 +0100

    feat: added ideal AD as config option to performance reward

--- environment/reward/syscall_performance_reward.py
+++ environment/reward/syscall_performance_reward.py
@@ -1,10 +1,12 @@
 import math
 import logging
+import json
 
 from environment.reward.abstract_reward import AbstractReward
 from environment.anomaly_detection.syscall_anomaly_detection import train_syscall_anomaly_detection, \
     detect_syscall_anomaly
 from environment.state_handling import collect_rate
+from config import config
 
 
 class SyscallPerformanceReward(AbstractReward):
@@ -12,17 +14,23 @@ class SyscallPerformanceReward(AbstractReward):
         self.r_done = r_done
         self.r_hidden = r_hidden
         self.r_detected = r_detected
+        self.use_ideal_AD = config.get_default_bool("anomaly_detection", "use_ideal_AD", False)
 
     @staticmethod
     def prepare_reward_computation():
         train_syscall_anomaly_detection()
 
-    def compute_reward(self, syscall_features, done):
+    def compute_reward(self, syscall_features, action, done):
         rate = collect_rate()
         logging.debug(f"REWARD: rate {rate}")
 
-        anomalous = detect_syscall_anomaly(syscall_features)[0] == -1  # int [-1, 1]
-        logging.info("--- Detected {} Syscall.".format("anomalous" if anomalous else "normal"))
+        if self.use_ideal_AD:
+            hidden_configs = json.loads(config.get("anomaly_detection", "hidden_configs"))
+            anomalous = bool(action not in hidden_configs)
+        else:
+            anomalous = detect_syscall_anomaly(syscall_features)[0] == -1  # int [-1, 1]
+
+        logging.debug("--- Detected {} Syscall.".format("anomalous" if anomalous else "normal"))
 
         if anomalous:
             logging.debug(f"REWARD: det {self.r_detected} {rate} {max(rate, 1)}")
