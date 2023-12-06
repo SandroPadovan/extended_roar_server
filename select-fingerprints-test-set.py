a6b26ba9ef89610b12df977753c6cec45c6f74d1
SandroPadovan <sandropadovan@bluewin.ch>
2023-12-06 11:16:12 +0100

    feat: adapted select-fingerprints-test-set.py script to syscalls
    - also: don't move files, but copy them

--- select-fingerprints-test-set.py
+++ select-fingerprints-test-set.py
@@ -1,5 +1,6 @@
 import os
 import random
+import shutil
 
 from environment.state_handling import get_num_configs
 from config import config
@@ -17,25 +18,25 @@ def move_files(origin_dir, evaluation_target, training_target):
     os.makedirs(evaluation_target, exist_ok=True)
     for i in range(int(0.2 * size)):
         file = random.choice(remaining_files)
-        os.rename(os.path.join(origin_dir, file), os.path.join(evaluation_target, file))
+        shutil.copyfile(os.path.join(origin_dir, file), os.path.join(evaluation_target, file))
         remaining_files.remove(file)
 
     os.makedirs(training_target, exist_ok=True)
     for file in remaining_files:
-        os.rename(os.path.join(origin_dir, file), os.path.join(training_target, file))
+        shutil.copyfile(os.path.join(origin_dir, file), os.path.join(training_target, file))
 
 
 for config in range(get_num_configs()):
     print("Moving config", config)
-    origin_dir = os.path.join(complete_dir, "infected-c{}".format(config))
-    evaluation_target = os.path.join(evaluation_dir, "infected-c{}".format(config))
-    training_target = os.path.join(training_dir, "infected-c{}".format(config))
+    origin_dir = os.path.join(complete_dir, "infected-c{}".format(config), "syscalls")
+    evaluation_target = os.path.join(evaluation_dir, "infected-c{}".format(config), "syscalls")
+    training_target = os.path.join(training_dir, "infected-c{}".format(config), "syscalls")
     move_files(origin_dir, evaluation_target, training_target)
 
 print("Moving normal")
-origin_dir = os.path.join(complete_dir, "normal")
-evaluation_target = os.path.join(evaluation_dir, "normal")
-training_target = os.path.join(training_dir, "normal")
+origin_dir = os.path.join(complete_dir, "normal", "syscalls")
+evaluation_target = os.path.join(evaluation_dir, "normal", "syscalls")
+training_target = os.path.join(training_dir, "normal", "syscalls")
 move_files(origin_dir, evaluation_target, training_target)
 
 print("Done")
