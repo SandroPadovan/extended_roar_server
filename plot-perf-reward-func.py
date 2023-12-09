19c5f2bf40c0dff06e534ecf059b4286aec0f034
SandroPadovan <sandropadovan@bluewin.ch>
2023-12-09 18:45:14 +0100

    feat: adapted reward function plot script

--- plot-perf-reward-func.py
+++ plot-perf-reward-func.py
@@ -8,14 +8,13 @@ import numpy as np
 from environment.state_handling import initialize_storage, cleanup_storage, get_storage_path
 
 
-def pos(r):
-    h = +0
-    return list(map(lambda v: 10 * math.log10(v + 1) + abs(h), r))  # log 10
-    # return list(map(lambda v: 10 * math.log(v + 1) + abs(h), r))  # log = log_e = ln
+def pos(r, h, s_1, s_2):
+    # hidden
+    return list(map(lambda v: s_1 * math.log(s_2 * v + 1) + abs(h), r))
 
 
-def neg(r):
-    d = -20
+def neg(r, d):
+    # detected
     return list(map(lambda v: (d/max(v, 1)) - abs(d), r))
 
 
@@ -24,16 +23,16 @@ try:
     use_plot_backend("template")
 
     x_lim = 500
-    description = "pos-{}--neg-{}--h{}--d{}--x-{}".format("10log", "1overX", "+0", "-20", x_lim)
-
+    description = "variant1+5"
     x = np.linspace(0, x_lim)
 
-    plt.plot(x, pos(x))
-    plt.plot(x, neg(x))
+    plt.plot(x, pos(x, 0, 10, 1), color="tab:blue", linestyle="dashed")     # variant 1 hidden
+    plt.plot(x, pos(x, 0, 100, 0.01), color="tab:blue")     # variant 5 hidden
+    plt.plot(x, neg(x, -20), color="tab:orange")        # variant 1 / 5 detected
 
     plt.ylabel("Reward")
     plt.xlabel("Encryption Rate")
-    plt.legend(["Hidden", "Detected"])
+    plt.legend(["Hidden Variant 1", "Hidden Variant 5", "Detected Variant 1 / 5"])
 
     fig_file = os.path.join(get_storage_path(), "reward-func={}.png".format(description))
     plt.savefig(fig_file)
