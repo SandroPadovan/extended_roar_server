db876e8fd81b9c0c29e269fdb31edfe6d7621630
SandroPadovan <sandropadovan@bluewin.ch>
2023-12-18 22:20:05 +0100

    feat: adapted activation function plot script

--- plot-activation-func.py
+++ plot-activation-func.py
@@ -7,13 +7,16 @@ from matplotlib import use as use_plot_backend
 from environment.state_handling import initialize_storage, cleanup_storage, get_storage_path
 
 
-def act1(x):
-    return list(map(lambda v: v if v > 0 else 0, x))  # ReLU
+def relu(x):
+    return list(map(lambda v: v if v > 0 else 0, x))
 
 
-def act2(x):
-    return 1 / (1 + np.exp(-x))  # Logistic
-    # return x / (1 + np.exp(-x))  # SiLU
+def logistic(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def silu(x):
+    return x / (1 + np.exp(-x))
 
 
 try:
@@ -21,17 +24,15 @@ try:
     use_plot_backend("template")  # required for matplotlib, throws error otherwise
 
     x_lim = 4
-    # description = "a1-{}--a2-{}--x-{}".format("ReLU", "SiLU", x_lim)
-    description = "a1-{}--a2-{}--x-{}".format("ReLU", "Logistic", x_lim)
+    description = "a1-{}--a2-{}--x-{}".format("Logistic", "SiLU", x_lim)
 
     x = np.linspace(-x_lim, x_lim, num=500)
 
-    plt.plot(x, act1(x))
-    plt.plot(x, act2(x))
-    # plt.plot(x, act2(x), c="red")
+    # plt.plot(x, relu(x))
+    plt.plot(x, logistic(x))
+    plt.plot(x, silu(x))
 
-    # plt.legend(["ReLU", "SiLU"])
-    plt.legend(["ReLU", "Logistic"])
+    plt.legend(["Logistic", "SiLU"])
 
     plt.xlim(-x_lim, x_lim)
     plt.grid()
